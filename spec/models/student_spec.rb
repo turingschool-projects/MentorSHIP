beed407c9623270cffb6d8130eafcbc241870837
Josh Washke <jwashke@icloud.com>
2016-07-21 18:42:43 -0600

    Add model spec to student to test that profile completed defaults to false

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "profile_completed" do
+    it "defaults to false when created" do
+      student = create(:student)
+
+      expect(student.profile_completed).to be(false)
+    end
+  end
 end
