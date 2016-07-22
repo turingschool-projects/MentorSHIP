eaaff1ac983502c7473126d234650eaf3f3fa5fd
Josh Washke <jwashke@icloud.com>
2016-07-21 18:57:07 -0600

    Create seed file for Students

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+cohort_1602 = Cohort.create(number: 1602)
+
+20.times do |n|
+  student = Student.new(profile_completed: true)
+  student.cohort = cohort_1602
+  user = User.new(
+    name: "student #{n}",
+    email: "student#{n}@turing.io",
+    phone_number: "555-555-5555",
+    slack_username: "student#{n}"
+  )
+  student.user = user
+  result = student.save
+  puts "Saving user #{n + 1} - result: #{result}"
+  puts student.errors.full_messages.join(', ') unless result
+end
