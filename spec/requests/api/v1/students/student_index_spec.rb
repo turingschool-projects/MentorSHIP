2d3aede29b1278fad86d4a892e3c7fc52f702e13
Josh Washke <jwashke@icloud.com>
2016-07-21 18:40:33 -0600

    Fix request spec to create students with completed profiles, add second spec to test that only students with completed profiles are bding returned

--- spec/requests/api/v1/students/student_index_spec.rb
+++ spec/requests/api/v1/students/student_index_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "GET /api/v1/students" do
   it "returns an array of all students" do
-    students = create_list(:student, 3)
+    students = create_list(:student, 3, profile_completed: true)
     student = students.first
 
     get "/api/v1/students"
@@ -18,5 +18,17 @@ RSpec.describe "GET /api/v1/students" do
     expect(recieved_students.first["phone_number"]).to eq(student.user.phone_number)
     expect(recieved_students.first["slack_username"]).to eq(student.user.slack_username)
     expect(recieved_students.first["cohort"]).to eq(student.cohort.number)
+  end
+
+  it "only returns students with completed profiles" do
+    create(:student, profile_completed: true)
+    create(:student, profile_completed: false)
+
+    get "/api/v1/students"
+    parsed_json = JSON.parse(response.body)
+    recieved_students = parsed_json["students"]
+
+    expect(response.status).to eq(200)
+    expect(recieved_students.count).to eq(1)
   end
 end
