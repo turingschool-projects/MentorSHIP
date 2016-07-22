5cdacec956e9429c5436b07639edcc339a84b3df
Josh Washke <jwashke@icloud.com>
2016-07-21 18:02:38 -0600

    Create cohort table; Add cohort ID to student table

--- db/migrate/20160722000019_create_students.rb
+++ db/migrate/20160722000019_create_students.rb
@@ -2,7 +2,6 @@ class CreateStudents < ActiveRecord::Migration[5.0]
   def change
     create_table :students do |t|
       t.references :user, foreign_key: true
-      t.references :cohort, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721232653) do
+ActiveRecord::Schema.define(version: 20160722000206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cohorts", force: :cascade do |t|
+    t.integer  "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "cohort_id"
+    t.index ["cohort_id"], name: "index_students_on_cohort_id", using: :btree
+    t.index ["user_id"], name: "index_students_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -24,4 +39,6 @@ ActiveRecord::Schema.define(version: 20160721232653) do
     t.datetime "updated_at",     null: false
   end
 
+  add_foreign_key "students", "cohorts"
+  add_foreign_key "students", "users"
 end
