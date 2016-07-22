fc708c7f9dab269314d2fd560f207c263436482c
Josh Washke <jwashke@icloud.com>
2016-07-21 20:02:57 -0600

    Add mentor show route to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :students, only: [:index, :show]
-      resources :mentors, only: [:index]
+      resources :mentors, only: [:index, :show]
     end
   end
 end
