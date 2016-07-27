07fc15179069a3cf4e249e63aa579e04cc226fb6
Josh Washke <jwashke@icloud.com>
2016-07-27 10:47:43 -0600

    Add Github OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     end
   end
   get "/auth/github", as: :github_login
-  get "/auth/github/callback", to: "sessions#new"
+  get "/auth/github/callback", to: "sessions#create"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722015014) do
+ActiveRecord::Schema.define(version: 20160727161642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160722015014) do
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.string   "github_avatar_url"
+    t.string   "github_url"
   end
 
   add_foreign_key "mentors", "mentor_timezones"
