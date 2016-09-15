787a97c5b9cf8737a243f5cf1884266057a300f8
Christopher Soden <karavol27@gmail.com>
2016-09-15 15:36:51 -0600

    ENV not working, helpers working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,23 +6,23 @@ class SessionsController < ApplicationController
       cookies[:token] = @user.token
       cookies[:email] = @user.email
       cookies[:authenticated] = true;
-      redirect_to "http://localhost:4200/welcome"
+      redirect_to ENV['PATH_BASE'] + "/welcome"
     else
-      session.clear
-      cookies.delete :session_id
-      cookies.delete :token
-      cookies.delete :email
-      cookies.delete :authenticated
-      redirect_to "http://localhost:4200/"
+      destroy_session
     end
   end
 
   def destroy
+    destroy_session
+  end
+
+  private
+  def destroy_session
     session.clear
     cookies.delete :session_id
     cookies.delete :token
     cookies.delete :email
     cookies.delete :authenticated
-    redirect_to "http://localhost:4200/"
+    redirect_to ENV['PATH_BASE']
   end
 end
