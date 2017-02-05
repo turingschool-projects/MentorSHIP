533b66eea770943c2ddacb56fc98471ef73336bd
Vido <vidoseaver@gmail.com>
2017-02-04 21:35:26 -0700

    Add Monkey

--- app/assets/javascripts/components/_about_body.js.jsx
+++ app/assets/javascripts/components/_about_body.js.jsx
@@ -1,17 +1,23 @@
 var AboutBody = React.createClass({
   render() {
     return (
-      <div className="container">
+      <div className="container ">
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
         <div className="row">
           <div className="col s4 about_anchor">
             <img src={"assets/about_anchor.png"} />
           </div>
           <div className="col s4 about_treasure">
             <img src={"assets/about_treasure.png"} />
-
           </div>
-
-          <div className="col s4"> Parrot</div>
+          <div className="col s4">
+            <img src={"assets/about_monkey.png"} />
+          </div>
         </div>
       </div>
     )
