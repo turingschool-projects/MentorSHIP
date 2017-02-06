c90237d803ae61857ee1842f3740642513a6a583
Megan Talbot <meganftalbot@gmail.com>
2017-02-06 08:50:41 -0700

    Add blue to mentorship name in navbar

--- app/assets/javascripts/components/_navbar.js.jsx
+++ app/assets/javascripts/components/_navbar.js.jsx
@@ -3,7 +3,7 @@ var Navbar = React.createClass({
     return (
       <nav>
        <div className="nav">
-        <a href="/" className="brand-logo">MENTORSHIP</a>
+        <a href="/" className="brand-logo">MENTOR<span className="ship-word-navbar">SHIP</span></a>
          <ul id="nav-mobile" className="right hide-on-med-and-down">
            <li><a href="/">Home</a></li>
            <li><a href="/about">How It Works</a></li>
