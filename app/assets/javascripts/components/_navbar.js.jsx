4a8fa71072134b8b5eff8c2b91d2e3e1b3781f64
Megan Talbot <meganftalbot@gmail.com>
2017-02-06 08:27:03 -0700

    Update navbar to be ClassName, and take out binding for console error

--- app/assets/javascripts/components/_navbar.js.jsx
+++ app/assets/javascripts/components/_navbar.js.jsx
@@ -2,14 +2,15 @@ var Navbar = React.createClass({
   render() {
     return (
       <nav>
-       <div class="nav-wrapper">
-         <a href="/" class="brand-logo">MENTORSHIP</a>
-         <ul id="nav-mobile" class="right hide-on-med-and-down">
+       <div className="nav">
+        <a href="/" className="brand-logo">MENTORSHIP</a>
+         <ul id="nav-mobile" className="right hide-on-med-and-down">
            <li><a href="/">Home</a></li>
            <li><a href="/about">How It Works</a></li>
          </ul>
        </div>
       </nav>
+
     )
   }
 });
