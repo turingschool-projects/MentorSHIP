fb14fddcbfb74d8b1e35eda24dfa389026b58768
Megan Talbot <meganftalbot@gmail.com>
2017-02-07 11:48:38 -0700

    Rename new filter variable to searchableMentorsInfo

--- app/assets/javascripts/components/mentors/_body.js.jsx
+++ app/assets/javascripts/components/mentors/_body.js.jsx
@@ -10,8 +10,8 @@ var Body = React.createClass({
   searchMentors(query){
     query = query.toLowerCase();
     let mentors = this.state.allMentors.filter((mentor) => {
-      searchableInfo = mentor.name.toLowerCase() + mentor.location.toLowerCase() + mentor.expertise.toLowerCase() + mentor.company.toLowerCase() + mentor.bio.toLowerCase()
-      return searchableInfo.includes(query)
+      searchableMentorsInfo = mentor.name.toLowerCase() + mentor.location.toLowerCase() + mentor.expertise.toLowerCase() + mentor.company.toLowerCase() + mentor.bio.toLowerCase()
+      return searchableMentorsInfo.includes(query)
     });
     this.setState({mentors: mentors})
   },
