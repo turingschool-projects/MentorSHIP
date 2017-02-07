ad31449171c2e54fa679f7aa615cd5a223d85dc3
Ben Pepper <bfpepper87@gmail.com>
2017-02-07 11:33:05 -0700

    Deletes white space.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,11 +11,8 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
 end
 
