14f1bfde6398b8192548d18c0089545d853fac10
Ben Pepper <bfpepper87@gmail.com>
2017-02-06 17:29:05 -0700

    Adds user spec for relationships. Wiating to figure out validations before making tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+
+  context "Relationships" do
+    it { should belong_to(:timezone) }
+  end
+
 end
