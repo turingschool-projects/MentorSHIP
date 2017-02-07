b843e9cdcf3dfce31d03d102a99cfcea144cd71d
Jon Kimble <jonathanbkimble@gmail.com>
2017-02-07 11:04:35 -0700

    Test mentors index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
