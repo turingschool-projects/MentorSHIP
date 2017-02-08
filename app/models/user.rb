8a80849c2c44bfad8cfabd48989ef99480ae3b0e
Ben Pepper <bfpepper87@gmail.com>
2017-02-07 19:42:23 -0700

    Adds delegations to User, Students, and Mentors.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   has_one :student
   has_one :mentor
 
-
+  delegate :avatar,
+           :first_name,
+           :last_name,
+           :email,
+           :slack,
+           :cohort, to: :census_profile
 end
