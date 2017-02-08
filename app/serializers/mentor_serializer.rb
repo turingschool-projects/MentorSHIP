c0cc61fada3762fc7c68ed14d2b4e6bb52cd9e4d
Ben Pepper <bfpepper87@gmail.com>
2017-02-08 16:12:50 -0700

    Updates mentor and student serializers.

--- app/serializers/mentor_serializer.rb
+++ app/serializers/mentor_serializer.rb
@@ -16,4 +16,11 @@ class MentorSerializer < ActiveModel::Serializer
   def last_active
     object.last_active.strftime("%A %d %b %Y %l:%M %p")
   end
+
+  def name
+    first = object.first_name
+    last = object.last_name
+    "#{first} #{last}"
+  end
+  
 end
