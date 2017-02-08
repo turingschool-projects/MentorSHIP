d2b693a7bbc05826d2ec81156d99707bc861456a
Ben Pepper <bfpepper87@gmail.com>
2017-02-07 19:31:15 -0700

    Adds Students table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208022811) do
+ActiveRecord::Schema.define(version: 20170208023039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20170208022811) do
     t.datetime "updated_at",  null: false
     t.index ["timezone_id"], name: "index_mentors_on_timezone_id", using: :btree
     t.index ["user_id"], name: "index_mentors_on_user_id", using: :btree
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_students_on_user_id", using: :btree
   end
 
   create_table "timezones", force: :cascade do |t|
@@ -46,4 +53,5 @@ ActiveRecord::Schema.define(version: 20170208022811) do
 
   add_foreign_key "mentors", "timezones"
   add_foreign_key "mentors", "users"
+  add_foreign_key "students", "users"
 end
