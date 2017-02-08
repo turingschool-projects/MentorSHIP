27b6ef948bcf3d81c37280d2b68b1fbb0706aa3d
Megan Talbot <meganftalbot@gmail.com>
2017-02-07 18:22:39 -0700

    Add edit componenet for dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,9 @@ class DashboardController < ApplicationController
 
   def edit
     token = User.find(params[:id]).token
-    @user = CensusService.new(token).get_user
+    user = CensusService.new(token).get_user
+    @user = [] << user
+    render component: "Edit", props: {user: @user}
     # call to github service with token we received
     # within service, get user data from census
     # display data in forms on view
