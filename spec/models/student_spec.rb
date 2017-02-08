9db2b6c47bccb0f16731437e3ecd88a23e55e9fe
Ben Pepper <bfpepper87@gmail.com>
2017-02-07 19:56:41 -0700

    Adds delegation tests to Mentors, Students, and Users. Adds CensusProfile class.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  it { should belong_to(:user)}
+  context "Relationships" do
+    it { should belong_to(:user)}
+  end
+
+  context "Delegations" do
+    it { should delegate_method(:avatar).to(:user) }
+    it { should delegate_method(:first_name).to(:user) }
+    it { should delegate_method(:last_name).to(:user) }
+    it { should delegate_method(:phone).to(:user) }
+    it { should delegate_method(:email).to(:user) }
+    it { should delegate_method(:slack).to(:user) }
+    it { should delegate_method(:bio).to(:user) }
+    it { should delegate_method(:last_active).to(:user) }
+    it { should delegate_method(:census_id).to(:user) }
+    it { should delegate_method(:token).to(:user) }
+    it { should delegate_method(:cohort).to(:user) }
+
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,14 @@ describe User, type: :model do
     it { should have_one(:mentor)}
   end
 
+  context "Delegations" do
+    it { should delegate_method(:avatar).to(:census_profile) }
+    it { should delegate_method(:first_name).to(:census_profile) }
+    it { should delegate_method(:last_name).to(:census_profile) }
+    it { should delegate_method(:email).to(:census_profile) }
+    it { should delegate_method(:slack).to(:census_profile) }
+    it { should delegate_method(:cohort).to(:census_profile) }
+
+  end
+
 end
