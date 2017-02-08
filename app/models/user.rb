b295052fb9501d36af11482cd0f0f263f7355a68
Ben Pepper <bfpepper87@gmail.com>
2017-02-07 19:35:29 -0700

    Adds relationship test at model level.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_one :student
+  has_one :mentor
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe User, type: :model do
 
   context "Relationships" do
-    it { should belong_to(:timezone) }
+    it { should have_one(:student)}
+    it { should have_one(:mentor)}
   end
 
 end
