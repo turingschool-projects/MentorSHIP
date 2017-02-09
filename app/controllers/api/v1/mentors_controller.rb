6c2d6b589d4c88bf86a5467fbc86608609af877d
Nate Allen <neight.allen@gmail.com>
2017-02-09 12:08:40 -0700

    Sending Mentor objects from Mentor index

--- app/controllers/api/v1/mentors_controller.rb
+++ app/controllers/api/v1/mentors_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::MentorsController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
 
   def index
-    render json: User.all
+    render json: Mentor.all
   end
 
   def show
