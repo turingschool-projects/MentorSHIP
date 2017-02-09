930555d55df18275b87168a64069f11e2e90161f
Nate Allen <neight.allen@gmail.com>
2017-02-09 12:22:42 -0700

    Added hard coded token back in for demos

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
           :cohort, to: :census_profile
 
   def census_profile
+    token = "5cd91c71ba86d702504b4bd4d3b3a1126584d4bf09df70b106a6d42bb45a7c75"
     @census_profile ||= CensusProfile.find(census_id, token)
   end
 
