28d45f4e4d5d64fb7e847573e70a64a27cd5631a
Ben Pepper <bfpepper87@gmail.com>
2017-02-08 18:12:40 -0700

    add current_user to application controller, add destroy and redirect to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,4 +9,9 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to edit_dashboard_path(user)
   end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
 end
