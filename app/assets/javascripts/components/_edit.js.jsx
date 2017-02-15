2bb3c569cc5556eb15a5996452ffb7d04ee12a36
Megan Talbot <meganftalbot@gmail.com>
2017-02-15 15:16:47 -0700

    Updated on edit method to try to refire ajax call

--- app/assets/javascripts/components/_edit.js.jsx
+++ app/assets/javascripts/components/_edit.js.jsx
@@ -27,41 +27,39 @@ var Edit = React.createClass({
     return (
       <div className="container">
         <div className="edit-page">
-        <h5>Welcome {user.first_name}! Please enter the rest of your information in order to accept mentees.</h5>
-          <div className="row">
-            <div className="col s4">
-              <img src={user.image_url} className='dashboard-pic'/>
-              <form action="#">
-                <p>
-                  <input type="checkbox" disabled="disabled"/>
-                  <label for="test5">Accepting Mentees</label>
-                </p>
-              </form>
+          <h5>Welcome {user.first_name}! Please enter the rest of your information in order to accept mentees.</h5>
+            <div className="row">
+              <div className="col s4">
+                <img src={user.image_url} className='dashboard-pic'/>
+                <form action="#">
+                  <p><input type="checkbox" disabled="disabled"/>
+                  <label for="test5">Accepting Mentees</label></p>
+                </form>
+              </div>
+              <div className="col s6">
+                <h6><span className="edit-headers">First Name:</span> {user.first_name} </h6>
+                <h6><span className="edit-headers">Last Name:</span> {user.last_name} </h6>
+                <h6><span className="edit-headers">Slack:</span> {user.slack} </h6>
+                <h6><span className="edit-headers">Email:</span> {user.email} </h6>
+                <h6><span className="edit-headers">Bio:</span></h6>
+                  <input type='text' onChange={ (e) => this.setState({ bio: e.target.value }) }
+                    defaultValue="Enter your bio" />
+                <h6><span className="edit-headers">Company:</span></h6>
+                  <input type='text' onChange={ (e) => this.setState({ company: e.target.value }) }
+                    defaultValue="Enter your company" />
+                <h6><span className="edit-headers">Position:</span></h6>
+                   <input type='text' onChange={ (e) => this.setState({ position: e.target.value }) }
+                    defaultValue="Enter your position" />
+                <h6><span className="edit-headers">Location:</span></h6>
+                   <input type='text' onChange={ (e) => this.setState({ location: e.target.value }) }
+                    defaultValue="Enter your location" />
+                <h6><span className="edit-headers">Expertise:</span></h6>
+                  <input type='text' onChange={ (e) => this.setState({ expertise: e.target.value }) }
+                    defaultValue="Enter your expertise" />
+                <button onClick={this.handleEdit}> <a href="/mentors">Submit</a> </button>
+              </div>
             </div>
-          <div className="col s6">
-          <h6><span className="edit-headers">First Name:</span> {user.first_name} </h6>
-          <h6><span className="edit-headers">Last Name:</span> {user.last_name} </h6>
-          <h6><span className="edit-headers">Slack:</span> {user.slack} </h6>
-          <h6><span className="edit-headers">Email:</span> {user.email} </h6>
-          <h6><span className="edit-headers">Bio:</span></h6>
-           <input type='text' onChange={ (e) => this.setState({ bio: e.target.value }) }
-            defaultValue="Enter your bio" />
-          <h6><span className="edit-headers">Company:</span></h6>
-           <input type='text' onChange={ (e) => this.setState({ company: e.target.value }) }
-            defaultValue="Enter your company" />
-          <h6><span className="edit-headers">Position:</span></h6>
-           <input type='text' onChange={ (e) => this.setState({ position: e.target.value }) }
-            defaultValue="Enter your position" />
-          <h6><span className="edit-headers">Location:</span></h6>
-             <input type='text' onChange={ (e) => this.setState({ location: e.target.value }) }
-              defaultValue="Enter your location" />
-            <h6><span className="edit-headers">Expertise:</span></h6>
-             <input type='text' onChange={ (e) => this.setState({ expertise: e.target.value }) }
-              defaultValue="Enter your expertise" />
-          <button onClick={this.handleEdit}> <a href="/mentors">Submit</a> </button>
           </div>
-        </div>
-        </div>
       </div>
     )
   }
