170bc23664a096ba22361203f212e6275237f3c3
Megan Talbot <meganftalbot@gmail.com>
2017-02-14 20:27:36 -0700

    Add sub header on about page

--- app/assets/javascripts/components/_about_index.js.jsx
+++ app/assets/javascripts/components/_about_index.js.jsx
@@ -3,6 +3,7 @@ var AboutIndex = React.createClass({
     return (
       <div>
         <h1 id="header-about">HOW IT WORKS</h1>
+        <h4 id="ahoy-about">ahoy, me hearties! </h4>
         <AboutBody />
       </div>
     )
