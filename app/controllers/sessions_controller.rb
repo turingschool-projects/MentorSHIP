4148bc3e7c0ce453f98e146b88084a19f06e6816
Edilene <edilene_cruz@yahoo.com.br>
2017-05-30 17:17:14 -0600

    Create mentor or student accordingly
    Before this students were never getting created, only users,
    and mentors were getting created through the seed file.
    
    Through OAuth we need some way to distinguish a student between a mentor.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,22 @@ class SessionsController < ApplicationController
   def create
     census_user_info = env["omniauth.auth"]
     user = User.find_or_create_by(census_id: census_user_info["uid"])
+    ## some logic we need to put here to determine if the user is a mentor or not
+    ##????
+
     user.token = census_user_info['info']['token']
     user.census_id = census_user_info['uid']
     user.save
+
     session[:user_id] = user.id
     if not_mentor?(census_user_info)
+      Student.find_or_create_by(user_id: user.id)
       redirect_to mentors_path
-    elsif user.mentor.company = nil
+    elsif user.mentor.company == nil
+      Mentor.find_or_create_by(user_id: user.id)
       redirect_to edit_dashboard_path(user)
     else
+      Mentor.find_or_create_by(user_id: user.id)
       redirect_to mentors_path
     end
   end
