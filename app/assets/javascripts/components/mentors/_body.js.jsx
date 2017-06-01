5587e0498d0f42566e4087d4d4b3926716dec9df
blackknight75 <devbox4714@gmail.com>
2017-05-31 23:37:47 -0600

    make function in bod.js.jsx that gets eligble users

--- app/assets/javascripts/components/mentors/_body.js.jsx
+++ app/assets/javascripts/components/mentors/_body.js.jsx
@@ -4,8 +4,27 @@ var Body = React.createClass({
     return { mentors: []}
   },
 
+  getEligibleUsers(){
+    $.ajax({
+      method: "GET",
+      url: `https://turing-census.herokuapp.com/api/v1/users?access_token=1b952b2a700df2ba4b0b6f446ec5c4b9c19ee5024d1b5f068e04bc2f3cdc9092`,
+      headers: {
+                    'Access-Control-Allow-Origin': '*'
+                }
+    }).then(function(data){
+      var eligibleUsers = data.map(function(user){
+        if(user.roles[0].name == "graduated" || user.roles[0].name == "staff") {
+          return user
+        } else {
+        }
+      })
+      debugger;
+    })
+  },
+
   componentDidMount() {
-    $.getJSON('/api/v1/mentors.json', (response) => { this.setState({ mentors: response, allMentors: response }) });
+    $.getJSON('/api/v1/mentors.json', (response) => { this.setState({ mentors: response, allMentors: response })
+    });
   },
 
   searchMentors(query){
@@ -18,11 +37,9 @@ var Body = React.createClass({
   },
 
   filterMentorsByGender(gender){
-    // debugger
     if (gender == "All") {
       return this.setState({mentors: this.state.allMentors})
     } else {
-      debugger
       let mentors = this.state.allMentors.filter((mentor) => {
         return mentor.gender === gender
       })
@@ -71,6 +88,7 @@ var Body = React.createClass({
         <div className= "col s2 pull-s10">
           <AcceptingStudentsFilter filterMentorsByAcceptingStudents={this.filterMentorsByAcceptingStudents}/>
         </div>
+        {this.getEligibleUsers()}
       </div>
     </div>
     )
