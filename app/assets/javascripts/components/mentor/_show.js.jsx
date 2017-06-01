ad2dc67ff3e133952fe2f7b1e1b71a36a698a624
Valerie Trudell <v.trudell@protonmail.com>
2017-06-01 15:47:27 -0600

    adds styling to skills

--- app/assets/javascripts/components/mentor/_show.js.jsx
+++ app/assets/javascripts/components/mentor/_show.js.jsx
@@ -7,8 +7,17 @@ var MentorShow = React.createClass({
     let id = this.props.id;
     let skills = this.props.skills
     let skillName = skills.forEach(function (skill) {
-      let tableRef = document.getElementById('skill-list')
-      $(tableRef).append(`<tr><td>${skill.name}</td></tr>`);
+
+      if (skill.skill_type == "Technical") {
+        let tableRef = document.getElementById("Technical")
+        $(tableRef).append(`<tr><td>${skill.name}</td></tr>`);
+      } else if (skill.skill_type == "Non-Technical") {
+        let tableRef = document.getElementById("Non-Technical")
+        $(tableRef).append(`<tr><td>${skill.name}</td></tr>`);
+      } else if (skill.skill_type == "Languages & Frameworks") {
+        let tableRef = document.getElementById("Languages & Frameworks")
+        $(tableRef).append(`<tr><td>${skill.name}</td></tr>`);
+      }
     })
 
     $.getJSON(`/api/v1/mentors/${id}`, function(mentor){
@@ -35,38 +44,57 @@ var MentorShow = React.createClass({
                   <img className="mentor-pic-icon" src={mentor.avatar} />
                 </span>
            <h3 id="mentor-show-name">{mentor.name}</h3>
-           <p>Last Active: {mentor.last_active} </p>
          </div>
              <div className="col s6">
                <div className="container">
                <div className="form-area-show">
                  <h6 id="location-show-header"><span className="edit-headers">Location:</span> {mentor.location} </h6>
-                 <h6><span className="edit-headers">Company:</span> {mentor.position} @ {mentor.company} </h6>
-                 <h6><span className="edit-headers">Position:</span> {mentor.position} </h6>
-                 <h6><span className="edit-headers">Expertise:</span>  {mentor.expertise}</h6>
-                 <h6><span className="edit-headers">About Me:</span> : {mentor.bio} </h6>
-                 <h6><span className="edit-headers">Email:</span> {mentor.email} </h6>
-                 <h6><span className="edit-headers">Slack:</span>  {mentor.slack} </h6>
-                 <h6><span className="edit-headers">Available:</span>  {" " +mentor.active} </h6>
-                  <div className='skills-table'>
-                    <table id="skill-list">
-                      <thead>
-                        <tr>
-                          <th>Skills: </th>
-                        </tr>
-                      </thead>
-                      <tbody >
-                        <tr>
-                          <td>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
+                 <h6><span className="edit-headers">Last Active: </span> {mentor.last_active} </h6>
+                 <h6><span className="edit-headers">Company: </span> {mentor.position} @ {mentor.company} </h6>
+                 <h6><span className="edit-headers">Position: </span> {mentor.position} </h6>
+                 <h6><span className="edit-headers">Expertise: </span>  {mentor.expertise}</h6>
+                 <h6><span className="edit-headers">About Me: </span> {mentor.bio} </h6>
+                 <h6><span className="edit-headers">Email: </span> {mentor.email} </h6>
+                 <h6><span className="edit-headers">Slack: </span>  {mentor.slack} </h6>
+                 <h6><span className="edit-headers">Available: </span>  {" " +mentor.active} </h6>
                 </div>
               </div>
            </div>
          </div>
+         <br/>
+         <br/>
+           <div className='skills-table'>
+             <h4> Skills</h4>
+             <table id="Technical" className='each-table'>
+              <thead>
+                <tr>
+                  <th className='table-center'>Technical</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+             </table>
+
+             <table id="Non-Technical" className='each-table'>
+              <thead>
+                <tr>
+                  <th className='table-center'>Non-Technical</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+             </table>
+
+             <table id="Languages & Frameworks" className='each-table'>
+              <thead>
+                <tr>
+                  <th className='table-center'>Languages & Frameworks</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+             </table>
+           </div>
          </div>
        </div>
 
