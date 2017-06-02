3afa0f86b1c590be597e08869346b1cb79829ebf
Edilene <edilene_cruz@yahoo.com.br>
2017-06-01 18:54:03 -0600

    updated tests

--- spec/models/student_mentor_spec.rb
+++ spec/models/student_mentor_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe StudentMentor, type: :model do
-  describe "#favorited_mentors" do
-    xit "brings back mentors where favorite is true" do
-
-    end
-  end
-
   context "Relationships" do
     it { should belong_to(:mentor) }
     it { should belong_to(:student) }
